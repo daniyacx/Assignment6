65ee1a1f6ab2da3b945dd1aee2407909632b7f10
aubak <aubakirovadania@gmail.com>
2023-06-01 08:27:35 +0600

    javadoc for Search interface

--- src/Search.java
+++ src/Search.java
@@ -1,5 +1,16 @@
 import java.util.List;
 
+/**
+ * search interface finds a path between two vertices in a graph
+ * @param <V> the type of data stored in the vertices
+ */
 public interface Search<V> {
+
+    /**
+     * @findPath finds a path between the source vertex and the destination vertex in a graph
+     * @param source      the source vertex
+     * @param destination the destination vertex
+     * @return a list of vertices representing the path from the source to the destination
+     */
     List<V> findPath(Vertex<V> source, Vertex<V> destination);
 }
