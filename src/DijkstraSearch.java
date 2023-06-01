bdb3b39109fdbaa5771bd8fc21c8ad15e3580124
aubak <aubakirovadania@gmail.com>
2023-06-01 08:53:32 +0600

    javadoc and comments for DijkstraSearch

--- src/DijkstraSearch.java
+++ src/DijkstraSearch.java
@@ -1,16 +1,25 @@
 import java.util.*;
-
 public class DijkstraSearch<V> implements Search<V> {
     private WeightedGraph<V> graph;
 
+    /**
+     * constructs a new DijkstraSearch object with the specified weighted graph
+     * @param graph the weighted graph to be searched
+     */
     public DijkstraSearch(WeightedGraph<V> graph) {
         this.graph = graph;
     }
 
+    /**
+     * performs Dijkstra's algorithm starting from the specified start vertex and returns the distances to all vertices
+     * @param startVertex the start vertex for the search
+     * @return a map containing the distances from the start vertex to all other vertices
+     */
     public Map<Vertex<V>, Double> dijkstraSearch(Vertex<V> startVertex) {
         Map<Vertex<V>, Double> distances = new HashMap<>();
         PriorityQueue<DijkstraNode<V>> priorityQueue = new PriorityQueue<>();
 
+        // initialize all distances as positive infinity except the start vertex
         for (Vertex<V> vertex : graph.getVertices()) {
             distances.put(vertex, vertex.equals(startVertex) ? 0.0 : Double.POSITIVE_INFINITY);
         }
@@ -22,6 +31,7 @@ public class DijkstraSearch<V> implements Search<V> {
             Vertex<V> currentVertex = currentNode.getVertex();
             double currentDistance = currentNode.getDistance();
 
+            // if the current distance is greater than the stored distance, skip processing
             if (currentDistance > distances.get(currentVertex)) {
                 continue;
             }
@@ -32,6 +42,7 @@ public class DijkstraSearch<V> implements Search<V> {
                 double edgeWeight = edge.getWeight();
                 double distanceThroughCurrent = currentDistance + edgeWeight;
 
+                // if the calculated distance is smaller than the stored distance, update the distance and enqueue the neighbor vertex
                 if (distanceThroughCurrent < distances.get(neighborVertex)) {
                     distances.put(neighborVertex, distanceThroughCurrent);
                     priorityQueue.offer(new DijkstraNode<>(neighborVertex, distanceThroughCurrent));
@@ -42,6 +53,10 @@ public class DijkstraSearch<V> implements Search<V> {
         return distances;
     }
 
+    /**
+     * @printDijkstra prints the result of Dijkstra's algorithm, including the distances from the start vertex to all other vertices
+     * @param startVertex the start vertex for the search
+     */
     public void printDijkstra(Vertex<V> startVertex) {
         Map<Vertex<V>, Double> distances = dijkstraSearch(startVertex);
 
@@ -53,11 +68,18 @@ public class DijkstraSearch<V> implements Search<V> {
         }
     }
 
+    /**
+     * @findPath finds the shortest path from the source vertex to the destination vertex using Dijkstra's algorithm
+     * @param source      the source vertex
+     * @param destination the destination vertex
+     * @return a list of vertices representing the shortest path from the source to the destination
+     */
     public List<V> findPath(Vertex<V> source, Vertex<V> destination) {
         Map<Vertex<V>, Double> distances = new HashMap<>();
         Map<Vertex<V>, Vertex<V>> parentMap = new HashMap<>();
         PriorityQueue<DijkstraNode<V>> priorityQueue = new PriorityQueue<>();
 
+        // initialize all distances as positive infinity except the source vertex
         for (Vertex<V> vertex : graph.getVertices()) {
             distances.put(vertex, vertex.equals(source) ? 0.0 : Double.POSITIVE_INFINITY);
         }
@@ -69,6 +91,7 @@ public class DijkstraSearch<V> implements Search<V> {
             Vertex<V> currentVertex = currentNode.getVertex();
             double currentDistance = currentNode.getDistance();
 
+            // if the current distance is greater than the stored distance, skip processing
             if (currentDistance > distances.get(currentVertex)) {
                 continue;
             }
@@ -79,6 +102,7 @@ public class DijkstraSearch<V> implements Search<V> {
                 double edgeWeight = edge.getWeight();
                 double distanceThroughCurrent = currentDistance + edgeWeight;
 
+                // if the calculated distance is smaller than the stored distance, update the distance and parentMap, and enqueue the neighbor vertex
                 if (distanceThroughCurrent < distances.get(neighborVertex)) {
                     distances.put(neighborVertex, distanceThroughCurrent);
                     parentMap.put(neighborVertex, currentVertex);
@@ -90,6 +114,12 @@ public class DijkstraSearch<V> implements Search<V> {
         return buildPath(parentMap, destination);
     }
 
+    /**
+     * @buildPath builds the shortest path from the source to the destination vertex using the parentMap
+     * @param parentMap   a map containing the parent vertices for each vertex in the path
+     * @param destination the destination vertex
+     * @return a list of vertices representing the shortest path from the source to the destination
+     */
     private List<V> buildPath(Map<Vertex<V>, Vertex<V>> parentMap, Vertex<V> destination) {
         List<V> path = new ArrayList<>();
         Vertex<V> currentVertex = destination;
@@ -102,19 +132,36 @@ public class DijkstraSearch<V> implements Search<V> {
         return path;
     }
 
+    /**
+     * this inner class represents a node in Dijkstra's algorithm with a vertex and a distance
+     * @param <V> the type of the vertex
+     */
     private static class DijkstraNode<V> implements Comparable<DijkstraNode<V>> {
         private Vertex<V> vertex;
         private double distance;
 
+        /**
+         * constructs a DijkstraNode with the specified vertex and distance
+         * @param vertex   the vertex associated with this node
+         * @param distance the distance associated with this node
+         */
         public DijkstraNode(Vertex<V> vertex, double distance) {
             this.vertex = vertex;
             this.distance = distance;
         }
 
+        /**
+         * @getVertex returns the vertex associated with this node
+         * @return the vertex associated with this node
+         */
         public Vertex<V> getVertex() {
             return vertex;
         }
 
+        /**
+         * @getDistance returns the distance associated with this node
+         * @return the distance associated with this node
+         */
         public double getDistance() {
             return distance;
         }
