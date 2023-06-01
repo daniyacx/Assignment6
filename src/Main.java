4ebd53019c669a17210942902fdd701e194d4c8b
aubak <aubakirovadania@gmail.com>
2023-06-01 08:58:47 +0600

    update main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         WeightedGraph<String> graph = new WeightedGraph<>();
-
+        // add vertices to the graph
         Vertex<String> a = new Vertex<>("Felix");
         Vertex<String> b = new Vertex<>("Jeongin");
         Vertex<String> c = new Vertex<>("Seungmin");
@@ -16,7 +16,7 @@ public class Main {
         graph.addVertex(d);
         graph.addVertex(e);
         graph.addVertex(f);
-
+        // add edges to the graph
         graph.addEdge(a, b, 4.0);
         graph.addEdge(a, c, 2.0);
         graph.addEdge(b, c, 1.0);
@@ -26,7 +26,7 @@ public class Main {
         graph.addEdge(d, e, 2.0);
         graph.addEdge(d, f, 6.0);
         graph.addEdge(e, f, 3.0);
-
+        // test BFS
         BreadthFirstSearch<String> bfs = new BreadthFirstSearch<>(graph);
         System.out.println("BreadthFirstSearch traversal:");
         List<Vertex<String>> bfsTraversal = bfs.BreadthFirstSearch(a);
@@ -34,7 +34,7 @@ public class Main {
             System.out.print(vertex.getData() + " ");
         }
         System.out.println();
-
+        // test Dijkstra's algorithm
         DijkstraSearch<String> dijkstraSearch = new DijkstraSearch<>(graph);
         System.out.println("Shortest distances using Dijkstra's algorithm:");
         Map<Vertex<String>, Double> shortestDistances = dijkstraSearch.dijkstraSearch(a);
@@ -43,7 +43,7 @@ public class Main {
             String distanceString = (distance == Double.POSITIVE_INFINITY) ? "Infinity" : String.valueOf(distance);
             System.out.println("Vertex: " + vertex.getData() + ", Distance: " + distanceString);
         }
-
+        // test finding the shortest path using Dijkstra's algorithm
         System.out.println("Shortest path from A to E:");
         List<String> shortestPath = dijkstraSearch.findPath(a, e);
         for (String vertexData : shortestPath) {
